e2e1883ec8a81b9648d68421d9804142de35e82f
Gmarques023 <153223582+Gmarques023@users.noreply.github.com>
2024-04-06 15:40:41 +0000

    update checkUserExists and createUser Functions

--- src/js/component/TodoList.jsx
+++ src/js/component/TodoList.jsx
@@ -1,42 +1,60 @@
 import React, { useState } from "react";
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faTrashCan } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faTrashCan } from "@fortawesome/free-solid-svg-icons";
 
 const TodoList = () => {
   const [userName, setUserName] = useState("");
   const [todoList, setTodoList] = useState([]);
-  const [newTodo, setNewTodo] = useState("") 
-  const [createUserName, setCreateUserName] = useState([]);
+  const [newTodo, setNewTodo] = useState("");
 
-  const checkUserNameExists = (userName) => {
-    return createUserName.includes(userName);
+  const checkUserNameExists = async (userName) => {
+    try {
+      const resp = await fetch(`https://playground.4geeks.com/todo/users/`, {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
+      if (!resp.ok) {
+        throw new Error(resp.status + "Something Went Wrong");
+      }
+      const data = await resp.json();
+      const userExists = data.users.some((user) => user.name === userName);
+      console.log("User exists:", userExists);
+      return userExists;
+    } catch (err) {
+      console.log(err);
+      return false; // Return false if an error occurs
+    }
   };
 
-  const createUser = () => {
-    if (!checkUserNameExists(userName)) {
-      setCreateUserName([...createUserName, userName]);
-      console.log("User created:", userName);
-      fetch (`https://playground.4geeks.com/todo/users/${userName}`, {
-        method: 'POST',
-        headers: {
-          "Content-Type": "application/json"
+  const createUser = async () => {
+    try {
+      const userExists = await checkUserNameExists(userName);
+      if (!userExists) {
+        console.log("User created:", userName);
+        const resp = await fetch(
+          `https://playground.4geeks.com/todo/users/${userName}`,
+          {
+            method: "POST",
+            headers: {
+              "Content-Type": "application/json",
+            },
+            body: JSON.stringify({ name: userName }),
+          }
+        );
+        if (!resp.ok) {
+          const data = await resp.json();
+          throw new Error(data.detail);
         }
-      }).then((resp) => {
-        if (resp.ok) {
-          return resp.json()
-        }
-        throw Error(resp.status + "Something Went Wrong")
-      }).then(()=>{
-        fetchTodoList()
-      }).catch((err) => {
-        console.log("Something Went Wrong!", err)
-      })
+      }
+    } catch (err) {
+      console.log(err.message);
     }
   };
 
   const fetchTodoList = () => {
     console.log("Fetch the username", userName);
-
     fetch(`https://playground.4geeks.com/todo/users/${userName}`, {
       method: "GET",
       headers: {
@@ -50,9 +68,8 @@ const TodoList = () => {
         throw Error(resp.status + "Something went wrong");
       })
       .then((userData) => {
-        console.log(userData)
+        console.log(userData);
         setTodoList(userData.todos);
-        
       })
       .catch((err) => {
         console.log(err);
@@ -60,65 +77,75 @@ const TodoList = () => {
   };
 
   const addNewTodo = () => {
-    
-    const newTodoList ={label: newTodo, is_done: false }
+    const newTodoList = { label: newTodo, is_done: false };
 
-    if (newTodo !== ''){
+    if (newTodo !== "") {
       fetch(`https://playground.4geeks.com/todo/todos/${userName}`, {
-        method: 'POST',
+        method: "POST",
         body: JSON.stringify(newTodoList),
         headers: {
-          "Content-Type": "application/json"
+          "Content-Type": "application/json",
         },
-      }).then(resp => {
-        if (resp.ok) {
-          return resp.json()
-        }
-        throw Error(resp.status + "Something went wrong in add todo");
-      }).then(() => {
-        fetchTodoList()
-        setNewTodo("")
-      }).catch((err) => {
-        console.log("Something went wrong", err);
       })
+        .then((resp) => {
+          if (resp.ok) {
+            return resp.json();
+          }
+          throw Error(resp.status + "Something went wrong in add todo");
+        })
+        .then(() => {
+          fetchTodoList();
+          setNewTodo("");
+        })
+        .catch((err) => {
+          console.log("Something went wrong", err);
+        });
     }
-  }
+  };
 
   const deleteTask = (taskId) => {
-    fetch (`https://playground.4geeks.com/todo/todos/${taskId}`, {
-      method: 'DELETE',
+    fetch(`https://playground.4geeks.com/todo/todos/${taskId}`, {
+      method: "DELETE",
       headers: {
-        "Content-Type":"application/json"
-      }
-    }).then(resp => {
-      if (resp.ok) {
-        return resp.json
-      }
-      throw Error(resp.status + "Something Went Wrong!")
-    }).then(()=>{
-      fetchTodoList()
-    }).catch((err)=>{
-      console.log("Something Went Wrong", err)
+        "Content-Type": "application/json",
+      },
     })
-  }
+      .then((resp) => {
+        if (resp.ok) {
+          return resp.json;
+        }
+        throw Error(resp.status + "Something Went Wrong!");
+      })
+      .then(() => {
+        fetchTodoList();
+      })
+      .catch((err) => {
+        console.log("Something Went Wrong", err);
+      });
+  };
 
   const deleteUser = (userName) => {
-    fetch (`https://playground.4geeks.com/todo/users/${userName}`, {
-      method: 'DELETE',
+    fetch(`https://playground.4geeks.com/todo/users/${userName}`, {
+      method: "DELETE",
       headers: {
-        "Content-Type": "application/json"
-      }
-    }).then(resp => {
-      if (resp.ok) {
-        return resp.json()
-      }
-      throw Error(resp.status + " Something Went Wrong")
-    }).then(() => {
-        setUserName("")
-    }).catch((err) => {
-      console.log("Something Went Wrong", err)
+        "Content-Type": "application/json",
+      },
     })
-  }
+      .then((resp) => {
+        if (resp.ok) {
+          return resp.json();
+        }
+        throw Error(resp.status + " Something Went Wrong");
+      })
+      .then(() => {
+        console.log(resp);
+        setUserName("");
+        console.log("user deleted");
+      })
+      .catch((err) => {
+        console.log("Something Went Wrong", err);
+      });
+  };
 
   return (
     <div>
@@ -129,25 +156,35 @@ const TodoList = () => {
           placeholder="Please, enter your username"
           onKeyDown={(e) => {
             if (e.key === "Enter") {
-              createUser()
-              fetchTodoList();
+              {
+                createUser();
+                fetchTodoList();
+              }
             }
           }}
         />
         <button onClick={() => deleteUser(userName)}>Delete User</button>
       </div>
-      <input 
+      <input
         value={newTodo}
-        onChange={e => setNewTodo(e.target.value)}
+        onChange={(e) => setNewTodo(e.target.value)}
         placeholder="Enter a new task!"
-        onKeyDown={e => {
-          if(e.key === 'Enter'){
-            addNewTodo()
+        onKeyDown={(e) => {
+          if (e.key === "Enter") {
+            addNewTodo();
           }
         }}
       />
       {todoList.map((todo, index) => {
-        return <div key={index}>{todo.label}<FontAwesomeIcon icon={faTrashCan} onClick={() => deleteTask(todo.id)}/></div>
+        return (
+          <div key={index}>
+            {todo.label}
+            <FontAwesomeIcon
+              icon={faTrashCan}
+              onClick={() => deleteTask(todo.id)}
+            />
+          </div>
+        );
       })}
     </div>
   );
